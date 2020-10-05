c53711f47dfdfff44d66f48c7d1f1c863d7da0d6
James Dale <git@jamesdale.com.au>
2020-10-06 03:13:29 +1100

    Initial Commit

---
+++ Ally/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ally
+//
+//  Created by James Dale on 6/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
